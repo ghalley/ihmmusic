95d0d4d6999a17c2510b29c17765bf185d5ba028
Gabriel Halley <gabhalley@gmail.com>
2016-03-26 17:50:21 -0400

    setup page for google calendar

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'posts#index'
 
+  get 'calendar' => 'application#calendar'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
