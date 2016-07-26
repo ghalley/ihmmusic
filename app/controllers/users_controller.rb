077ef291e22dadb27b5fe0bd8e5d35776b38e46d
Gabriel Halley <gabhalley@gmail.com>
2016-07-26 07:53:13 -0400

    stricter user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -60,6 +60,7 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation
+                                    :street_address1, :street_address2, :city, :state, :zip_code)
     end
 end
