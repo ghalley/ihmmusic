2a4c61e42eb0ae7f1a3e4d210acfbc87d04c4aab
Gabriel Halley <gabhalley@gmail.com>
2016-07-26 08:29:05 -0400

    update admin role through form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
   before_action :authenticate_user!
+  before_action :set_roles, only: [:create, :update]
 
   # GET /users
   # GET /users.json
@@ -41,6 +42,7 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
+    clean_passwords
     respond_to do |format|
       if @user.update(user_params)
         format.html { redirect_to users_path, flash: {success: 'User was successfully updated.'} }
@@ -60,7 +62,19 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation
-                                    :street_address1, :street_address2, :city, :state, :zip_code)
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation,
+                                    :street_address1, :street_address2, :city, :state, :zip_code, :role)
+    end
+
+    def clean_passwords
+      params[:user].except!(:password, :password_confirmation)
+    end
+
+    def set_roles
+      if params[:role].present?
+        @user.add_role params[:role].to_sym
+      elsif @user.has_role? :admin
+        @user.remove_role :admin
+      end
     end
 end
